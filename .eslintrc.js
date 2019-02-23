e8d3d0bab9e4c8591c3647506d07b60ccdefd9aa
Léo Lam <leo@innovatetechnologi.es>
2019-02-23 17:05:19 +0100

    Update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,4 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
-  parserOptions: {
-    parser: 'typescript-eslint-parser'
-  }
 }
