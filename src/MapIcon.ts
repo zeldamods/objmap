97649f2f057dadda4f255cea196df9b4486ffce9
Léo Lam <leo@innovatetechnologi.es>
2019-02-23 16:19:27 +0100

    MapIcon: Use L.Point for sizes
    Some plugins expect L.Points for icon sizes.

--- src/MapIcon.ts
+++ src/MapIcon.ts
@@ -2,72 +2,72 @@ import * as L from 'leaflet';
 
 export const DUNGEON = L.icon({
   iconUrl: '/icons/mapicon_dungeon.svg',
-  iconSize: [36, 36],
+  iconSize: L.point(36, 36),
   className: 'mapicon-Dungeon',
 });
 export const DUNGEON_DLC = L.icon({
   iconUrl: '/icons/mapicon_dungeon_dlc.svg',
-  iconSize: [36, 36],
+  iconSize: L.point(36, 36),
   className: 'mapicon-Dungeon',
 });
 
 
 export const VILLAGE = L.icon({
   iconUrl: '/icons/mapicon_village.svg',
-  iconSize: [32, 32],
+  iconSize: L.point(32, 32),
   tooltipAnchor: [0, 20],
 });
 export const CHECKPOINT = L.icon({
   iconUrl: '/icons/mapicon_checkpoint.svg',
-  iconSize: [26, 26],
+  iconSize: L.point(26, 26),
   tooltipAnchor: [0, 18],
 });
 export const HATAGO = L.icon({
   iconUrl: '/icons/mapicon_hatago.svg',
-  iconSize: [32, 32],
+  iconSize: L.point(32, 32),
   tooltipAnchor: [0, 20],
 });
 export const CASTLE = L.icon({
   iconUrl: '/icons/mapicon_castle.svg',
-  iconSize: [32, 32],
+  iconSize: L.point(32, 32),
   tooltipAnchor: [0, 20],
 });
 
 
 export const TOWER = L.icon({
   iconUrl: '/icons/mapicon_tower.svg',
-  iconSize: [40, 40],
+  iconSize: L.point(40, 40),
   className: 'mapicon-Tower',
 });
 export const LABO = L.icon({
   iconUrl: '/icons/mapicon_labo.svg',
-  iconSize: [32, 32],
+  iconSize: L.point(32, 32),
 });
 
 
 export const SHOP_BOUGU = L.icon({
   iconUrl: '/icons/mapicon_shop_bougu.svg',
-  iconSize: [32, 32],
+  iconSize: L.point(32, 32),
 });
 export const SHOP_COLOR = L.icon({
   iconUrl: '/icons/mapicon_shop_color.svg',
-  iconSize: [32, 32],
+  iconSize: L.point(32, 32),
 });
 export const SHOP_JEWEL = L.icon({
   iconUrl: '/icons/mapicon_shop_jewel.svg',
-  iconSize: [32, 32],
+  iconSize: L.point(32, 32),
 });
 export const SHOP_YADOYA = L.icon({
   iconUrl: '/icons/mapicon_shop_yadoya.svg',
-  iconSize: [32, 32],
+  iconSize: L.point(32, 32),
 });
 export const SHOP_YOROZU = L.icon({
   iconUrl: '/icons/mapicon_shop_yorozu.svg',
-  iconSize: [32, 32],
+  iconSize: L.point(32, 32),
 });
 
 
 export const KOROK = L.icon({
   iconUrl: '/icons/mapicon_korok.png',
-  iconSize: [20, 20],
+  iconSize: L.point(20, 20),
 });
