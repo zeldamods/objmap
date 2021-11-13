0ee310d62e08239d31fea7c24bb87dfc47450af8
Brian Savage <savage13@gmail.com>
2021-11-13 06:19:50 -0500

    Update static.json generator to include korok ID data

--- tools/map_gen_markers.py
+++ tools/map_gen_markers.py
@@ -35,6 +35,7 @@ map_dir = game_files_dir / 'map'
 
 mainfield_static = json.load(open(map_dir/'MainField'/'Static.json', 'r'))
 mainfield_location = json.load(open(map_dir/'MainField'/'Location.json', 'r'))
+korok_data = json.load(open('korok_ids.json', 'r'))
 
 mainfield_markers: defaultdict = defaultdict(list)
 for l in mainfield_static['LocationMarker']:
@@ -58,7 +59,7 @@ data['markers']['Place'] = make_markers(chain(*(mainfield_markers[x] for x in ('
 data['markers']['Tower'] = make_markers(mainfield_markers['Tower'])
 data['markers']['Labo'] = make_markers(mainfield_markers['Labo'])
 data['markers']['Shop'] = make_markers(chain(*(mainfield_markers[x] for x in ('ShopBougu', 'ShopColor', 'ShopJewel', 'ShopYadoya', 'ShopYorozu'))))
-data['markers']['Korok'] = mainfield_static['KorokLocation']
+data['markers']['Korok'] = korok_data
 
 with open(game_files_dir / 'map_summary' / 'MainField' / 'static.json', 'w') as f:
     json.dump(data, f, ensure_ascii=False)
