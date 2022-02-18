35ba2680e8f01055b1212dba654d4dcb3e1a8ffe
Kaedenn <kaedenn@gmail.com>
2022-02-18 14:30:49 -0900

    Implement capitalization suggestion

--- src/components/ObjectInfo.vue
+++ src/components/ObjectInfo.vue
@@ -14,7 +14,7 @@
       <span style="color: #ff3915" v-if="data.hard_mode"><i class="fa fa-shield-alt fa-fw"></i> Master Mode</span>
     </section>
     <section class="search-result-id">
-      <span style="color: #ff3915" v-if="data.one_hit_mode"><i class="fa fa-shield-alt fa-fw"></i> One-hit Obliterator Object</span>
+      <span style="color: #ff3915" v-if="data.one_hit_mode"><i class="fa fa-shield-alt fa-fw"></i> One-Hit Obliterator Object</span>
     </section>
     <section class="search-result-drop" v-if="!dropAsName && data.drop"><i class="fa fa-infinity fa-fw"></i> {{drop()}}</section>
     <section class="search-result-equip" v-if="data.equip"><i class="fa fa-shield-alt fa-fw"></i> {{data.equip.map((x) => getName(x)).join(', ')}}</section>
