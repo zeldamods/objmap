9f3f2ae609e236a62b93de309dbe93aad58da086
Brian Savage <savage13@gmail.com>
2022-05-27 18:59:27 -0400

    Add enemy lookup for shrines

--- src/components/AppMapDetailsDungeon.vue
+++ src/components/AppMapDetailsDungeon.vue
@@ -3,10 +3,19 @@
     <h2 class="location-sub">{{sub}}</h2>
     <p>Dungeon number: {{marker.data.dungeonNum}}</p>
     <hr>
-    <h4 class="subsection-heading">Treasure Chests</h4>
-    <div class="search-results">
-      <ObjectInfo v-for="tbox in tboxObjs" :key="tbox.objid" :obj="tbox" drop-as-name />
-    </div>
+    <section v-if="tboxObjs.length">
+      <h4 class="subsection-heading">Treasure Chests</h4>
+      <div class="search-results">
+        <ObjectInfo v-for="tbox in tboxObjs" :key="tbox.objid" :obj="tbox" drop-as-name />
+      </div>
+    </section>
+    <section v-if="enemies.length">
+      <h4 class="subsection-heading">Enemies</h4>
+      <div class="search-results">
+        <ObjectInfo v-for="enemy in enemies" :key="enemy.objid" :obj="enemy" />
+      </div>
+    </section>
+
   </section>
 </template>
 <style lang="less">
