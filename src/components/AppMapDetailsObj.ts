566ec2cb5990a06148e0f26e4a19cc15106cca5c
savage13 <savage13@gmail.com>
2022-06-14 09:19:36 -0400

    Add Korok Moving Lights representations (#96)

--- src/components/AppMapDetailsObj.ts
+++ src/components/AppMapDetailsObj.ts
@@ -10,9 +10,11 @@ import ObjectInfo from '@/components/ObjectInfo';
 import { MapMgr, ObjectData, ObjectMinData, PlacementLink } from '@/services/MapMgr';
 import { MsgMgr } from '@/services/MsgMgr';
 import * as ui from '@/util/ui';
-const KUH_TAKKAR_ELEVATOR_HASH_ID = 0x96d181a0;
 
+import * as curves from '@/util/curves';
 import * as svg from '@/util/svg';
+
+const KUH_TAKKAR_ELEVATOR_HASH_ID = 0x96d181a0;
 
 const rock_target = ["Obj_LiftRockWhite_Korok_A_01", "Obj_LiftRockGerudo_Korok_A_01", "Obj_LiftRockEldin_Korok_A_01"];
 const rock_source = ["Obj_LiftRockWhite_A_01", "Obj_LiftRockGerudo_A_01", "Obj_LiftRockEldin_A_01"];
@@ -102,6 +104,7 @@ export default class AppMapDetailsObj extends AppMapDetailsBase<MapMarkerObj | M
   private staticData = staticData;
 
   private korokMarkers: any[] = [];
+  private rails: { [key: string]: any }[] = [];
 
   async init() {
     this.minObj = this.marker.data.obj;
@@ -114,6 +117,7 @@ export default class AppMapDetailsObj extends AppMapDetailsBase<MapMarkerObj | M
     this.linkTagInputs = [];
     this.areaMarkers.forEach(m => m.remove());
     this.areaMarkers = [];
+    this.rails = [];
     if (this.minObj.objid) {
       this.obj = (await MapMgr.getInstance().getObjByObjId(this.minObj.objid))!;
     } else {
@@ -128,6 +132,10 @@ export default class AppMapDetailsObj extends AppMapDetailsBase<MapMarkerObj | M
     this.genGroup = await MapMgr.getInstance().getObjGenGroup(this.obj.map_type, this.obj.map_name, this.obj.hash_id);
     for (const obj of this.genGroup) {
       this.genGroupSet.set(obj.hash_id, obj);
+    }
+
+    if (this.obj.data.LinksToRail) {
+      this.rails = await MapMgr.getInstance().getObjRails(this.obj.hash_id);
     }
 
     this.initLinks();
@@ -491,9 +499,11 @@ export default class AppMapDetailsObj extends AppMapDetailsBase<MapMarkerObj | M
   }
 
   initKorokMarkers() {
+    if (!this.obj)
+      return;
     const use_icon = true;
     let map = this.marker.data.mb;
-    if (this.obj && this.obj.korok_type == "Goal Ring (Race)") {
+    if (this.obj.korok_type == "Goal Ring (Race)") {
       let names = ["FldObj_KorokStartingBlock_A_01", "FldObj_KorokGoal_A_01"];
       let objs = this.genGroup.filter((obj: any) => names.includes(this.getName(obj.name)));
       // Start and End Markers
@@ -504,12 +514,18 @@ export default class AppMapDetailsObj extends AppMapDetailsBase<MapMarkerObj | M
       let ll = objs.map((obj: any) => [obj.data.Translate[2], obj.data.Translate[0]]);
       let line = L.polyline(ll, { color: '#cccccc', weight: 1.5 }).addTo(map.m);
       this.korokMarkers.push(line);
-    } else if (this.obj && this.obj.korok_type == "Rock Pattern") {
+    } else if (this.obj.korok_type == "Moving Lights") {
+      this.rails.forEach((rail: any) => {
+        let pts = curves.railPath(rail).map((pt: any) => [pt[2], pt[0]]);
+        let line = L.polyline(pts, { color: "#cccccc", weight: 2.0 }).addTo(map.m);
+        this.korokMarkers.push(line);
+      });
+    } else if (this.obj.korok_type == "Rock Pattern") {
       const rocks = [...rock_target, ...rock_source];
       let objs = this.genGroup.filter((obj: any) => rocks.includes(this.getName(obj.name)))
       let markers = objs.map((obj: any) => this.getKorokMarkerWithIcon(obj).addTo(map.m));
       this.korokMarkers.push(...markers);
-    } else if (this.obj && this.obj.korok_type == "Flower Trail") {
+    } else if (this.obj.korok_type == "Flower Trail") {
       let group = this.genGroup;
       let start = group.find((g: any) => this.getName(g.name) == "Obj_Plant_Korok_A_01" &&
         g.data['!Parameters'].IsNoAppearEffect);
---
+++ src/util/curves.ts
@@ -0,0 +1,128 @@
+
+function range(n: number): number[] {
+  return [...Array(n)].map((item: any, index: number) => index);
+}
+
+// Calculate Bezier coefficients: A row of Pascal's Triangle
+function bezierCoeff(n: number): number[] {
+  let row = Array(n + 1).fill(0);
+  row[0] = 1;
+  for (let i = 0; i <= n; i++) {
+    for (let j = i; j > 0; j--) {
+      row[j] += row[j - 1];
+    }
+  }
+  return row;
+}
+
+// dot-product
+function dot(a: number[], b: number[]): number {
+  let x = 0;
+  for (let i = 0; i < a.length; i++) {
+    x += a[i] * b[i];
+  }
+  return x;
+}
+
+// Point addition
+function ptAdd(a: number[], b: number[]): number[] {
+  return [a[0] + b[0], a[1] + b[1], a[2] + b[2]]
+}
+
+// Bezier Curve
+//
+// B(t) =  \Sum_{i=0}^{n} coeff(i,n) (1 - t)^{n-i} t^i P_i
+//
+// n = Number of points - 1 ; (n is inclusive)
+// All segments are divided into 36 segments, t = 0 to 1 ; dt = 1/36
+// Curve construction is performed on all components [x,y,z]
+//
+function bezier(pts: any) {
+  let steps = 36;
+  let out = [];
+  let n = pts.length;
+  if (n == 2) {
+    return pts;
+  }
+  const coeff = bezierCoeff(n - 1);
+
+  let t0 = 0;
+  let dt = 1.0 / (steps - 1);
+  let xi = pts.map((p: any) => p[0]);
+  let yi = pts.map((p: any) => p[1]);
+  let zi = pts.map((p: any) => p[2]);
+  for (let k = 0; k < steps; k++) {
+    const t = t0 + k * dt;
+    const ti = range(n).map((i: number) => coeff[i] * Math.pow(1.0 - t, n - 1 - i) * Math.pow(t, i));
+    for (let i = 0; i < n; i++) {
+      let pt = [dot(ti, xi), dot(ti, yi), dot(ti, zi)];
+      out.push(pt);
+    }
+  }
+  return out;
+}
+
+// Linear Rail path without any Control Points
+function railPathLinear(rail: any): any {
+  let pts = rail.RailPoints.map((pt: any) => pt.Translate);
+  if (rail['IsClosed']) {
+    pts.push(pts[0])
+  }
+  return pts;
+}
+
+// Bezier Path with Control Points
+// Each true is assumed to have two control points associated with it
+// The 2nd [1] control point builds the curve towards the next distinct point
+// The 1st [0] control point builds the curve towards the previous distinct point
+//
+//       b1 ____ c0
+//         /    \
+//  a     b      c     d
+//   \___/        \___/
+// a1    b0      c1    d0
+//
+//  Points: a, b, c, d
+//  Control points: a1, b0, b1, c0, c1, d0
+//
+//  Loop over sections of the bezier curve
+//   - Build: [b, b+b1, c+c0, c] that is passed to bezier()
+//   - Append bezier() output to the curve
+//  If the curve is closed, the the first point as a last point
+//  If the curve is open, do not use the last point as a curve starting point
+//
+function railPathBezier(rail: any): any {
+  let out = [];
+  let n = rail.RailPoints.length;
+  if (!rail['IsClosed']) {
+    n -= 1;
+  }
+
+  for (let i = 0; i < n; i++) {
+    let j = (i + 1);
+    if (rail['IsClosed']) {
+      j = j % n;
+    }
+    let p0 = rail.RailPoints[i].Translate;
+    let p1 = rail.RailPoints[j].Translate;
+    let bez = [p0];
+    if (rail.RailPoints[i].ControlPoints) {
+      bez.push(ptAdd(p0, rail.RailPoints[i].ControlPoints[1]));
+    }
+    if (rail.RailPoints[j].ControlPoints) {
+      bez.push(ptAdd(p1, rail.RailPoints[j].ControlPoints[0]));
+    }
+    bez.push(p1);
+    out.push(...bezier(bez))
+  }
+  return out;
+}
+
+export function railPath(rail: any): any {
+  if (rail.RailType == "Linear") {
+    return railPathLinear(rail);
+  } else if (rail.RailType == "Bezier") {
+    return railPathBezier(rail);
+  }
+  return null;
+}
