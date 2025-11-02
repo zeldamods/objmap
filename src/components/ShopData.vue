7d73f157d1073944d360889b5a0ec4f7d73abc1c
Brian Savage <savage13@gmail.com>
2025-11-02 15:41:30 -0500

    Futher updates to select background color

--- src/components/ShopData.vue
+++ src/components/ShopData.vue
@@ -27,10 +27,13 @@
   .table-row .cell {
       display: table-cell;
   }
-  .select_table {
+  select {
       color: white;
-      background: rgba(0,0,0,0.2);
+      background: rgba(0,0,0,0.7);
       border: rgba(255,255,255,0.2) 1px solid;
+  }
+  select option {
+      background: rgba(0,0,0,0.7);
   }
 
 </style>
