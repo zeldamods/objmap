45e8d0e585dca43183e5bbd1440d230a4cc8222d
Brian Savage <savage13@gmail.com>
2025-11-02 14:38:10 -0500

    Remove some background for select

--- src/components/ShopData.vue
+++ src/components/ShopData.vue
@@ -31,13 +31,6 @@
       color: white;
       background: rgba(0,0,0,0.2);
       border: rgba(255,255,255,0.2) 1px solid;
-
-      -webkit-appearance: none; /* For Chrome, Safari */
-      -moz-appearance: none;    /* For Firefox */
-      appearance: none;         /* Standard property */
-      background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 8 8"><polygon points="0,2 4,6 8,2" fill="white"/></svg>') no-repeat right 10px center;
-      background-size: 10px;
-      padding-right: 30px;   /* Make space for the custom arrow */
   }
 
 </style>
